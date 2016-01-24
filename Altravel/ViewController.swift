3f48585969bc20f1cbcd01080744d27988eb74b4
Courtney Machi <courtney.machi@gmail.com>
2016-01-23 17:32:12 -0800

    Styling login page

--- Altravel/ViewController.swift
+++ Altravel/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import Parse
-import ParseFacebookUtilsV4;
+import ParseFacebookUtilsV4
+import QuartzCore;
 
 class ViewController: UIViewController {
 
   
     @IBOutlet weak var backgroundImg: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +25,12 @@ class ViewController: UIViewController {
 //        blurView.frame = backgroundImg.bounds
 //        backgroundImg.addSubview(blurView)
 //        backgroundImg.frame = CGRectMake(0, 0, 100, 100)
-//        
+        
+        self.titleLabel.layer.masksToBounds = true
+        self.titleLabel.layer.cornerRadius = self.titleLabel.frame.size.height / 2;
         
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -35,17 +40,22 @@ class ViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated);
         
+
+        
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        
         if PFUser.currentUser() != nil {
             if PFFacebookUtils.isLinkedWithUser(PFUser.currentUser()!) {
                 self.performSegueWithIdentifier("loginSuccessSegue", sender: self)
             }
         }
     }
+    
 
+ 
 
     @IBAction func onLogin(sender: UIButton) {
         let permissions = ["public_profile"]
