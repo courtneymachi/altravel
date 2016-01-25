453a5ccbd5d9b42faa12118c59aa778a532192c7
Courtney Machi <courtney.machi@gmail.com>
2016-01-24 19:01:04 -0800

    tab nav and trip page storyboard

---
+++ Altravel/TripsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TripsViewController.swift
+//  Altravel
+//
+//  Created by courtney machi on 1/24/16.
+//  Copyright © 2016 courtney machi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class TripsViewController: UIViewController {
+    
+    
+    @IBAction func addTripButton(sender: UIButton) {
+        
+        self.performSegueWithIdentifier("addTripSuccessSegue", sender: self)
+    }
+    
+    
+
+}
