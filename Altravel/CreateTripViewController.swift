5dc966879b41cef7ef0d7581a79f3d4dfb991adc
Courtney Machi <courtney.machi@gmail.com>
2016-02-20 16:17:53 -0800

    create trip page details

--- Altravel/CreateTripViewController.swift
+++ Altravel/CreateTripViewController.swift
@@ -9,36 +9,90 @@
 import Foundation
 import UIKit
 
-class CreateTripViewController: UIViewController {
+class CreateTripViewController: UIViewController, UITextFieldDelegate {
+    
+    var date: NSDate!
+    var currentDateField: UITextField?
+    // IBOutlets
     
     
+    @IBOutlet weak var tripNameField: UITextField!
+    @IBOutlet weak var tripDetailsField: UITextField!
     @IBOutlet weak var startDateField: UITextField!
     @IBOutlet weak var endDateField: UITextField!
     
-    
-    @IBAction func startDateEditing(sender: UITextField) {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tripNameField.delegate = self
+        self.tripDetailsField.delegate = self
+        self.startDateField.delegate = self
+        self.endDateField.delegate = self
+        self.date = NSDate()
         
-        let datePickerView:UIDatePicker = UIDatePicker()
-        datePickerView.datePickerMode = UIDatePickerMode.Date
-        sender.inputView = datePickerView
-        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
-        }
-  
-    @IBAction func endDateEditing(sender: UITextField) {
-        let datePickerView:UIDatePicker = UIDatePicker()
-        datePickerView.datePickerMode = UIDatePickerMode.Date
-        sender.inputView = datePickerView
-        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        self.currentDateField = startDateField
+        
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = UIDatePickerMode.Date
+        startDateField.inputView = datePicker
+        endDateField.inputView = datePicker
+        datePicker.addTarget(self, action: "datePickerChanged:", forControlEvents: .ValueChanged)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.currentDateField = startDateField
+        self.date = NSDate()
+        displayDate(self.date)
+    }
+
+    
+    // IBActions
+    
+    @IBAction func saveButtonTapped(sender: UIButton) {
+        // TODO: save the trip by creating a new trip page
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    @IBAction func cancelButtonTapped(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
     
-    func datePickerValueChanged(sender:UIDatePicker) {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
-        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
-        startDateField.text = dateFormatter.stringFromDate(sender.date)
-        endDateField.text = dateFormatter.stringFromDate(sender.date)
-
+    // functions
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        textField.isFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing( textField: UITextField) {
+        self.currentDateField = textField;
+    } 
+    
+    func datePickerChanged(sender:UIDatePicker) {
+        displayDate(sender.date)
+    }
+    
+    func displayDate(date: NSDate) {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        self.currentDateField!.text = formatter.stringFromDate(date)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // dispose of any resources that can be recreated.
     }
     
 
