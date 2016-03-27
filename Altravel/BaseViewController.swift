cc5e5b925635ea4e4841d412710e5450eae0a542
Giuseppe Macrì <macri.giuseppe@gmail.com>
2016-03-27 13:52:51 -0700

    Created base view controller that will be used to extend existing controllers

---
+++ Altravel/BaseViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseViewController.swift
+//  Altravel
+//
+//  Created by Giuseppe Macrì on 3/27/16.
+//  Copyright © 2016 courtney machi. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController : UIViewController {
+    override func viewWillAppear(animated: Bool) {
+        
+    }
+
+}
